ec3a3be10e7a95dc3aa0feb259e081aeab5c11e0
ggandryxagg-star <ggandryxagg@gmail.com>
2026-03-02 22:22:55 +0500

    crestik

--- crestik.py
+++ crestik.py
@@ -10,15 +10,15 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    btn = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn1 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn2 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn3 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn4 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn5 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn6 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn7 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
-    btn8 = ft.Button('', icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150)
+    btn = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn1 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn2 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn3 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn4 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn5 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn6 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn7 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn8 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
     
     page.add(btn, btn1, btn2)
 
