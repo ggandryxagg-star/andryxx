5d6ef16b260e421b99c86d160750521f9d1710c2
ggandryxagg-star <ggandryxagg@gmail.com>
2026-03-04 15:09:56 +0500

    112

--- crestik.py
+++ crestik.py
@@ -3,14 +3,12 @@ import flet as ft
 
 def main(page: ft.Page):
     
-    page.title = 'Крестики_нолики'
+    page.title = 'Крестики нолики'
 
     page.window.height = 900
     page.window.width = 900
 
-    
-
-    btn = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.START)
+    btn = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=100, height=100, icon_size=100, alignment=ft.MainAxisAlignment.START)
     btn1 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.CENTER)
     btn2 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.END)
     btn3 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.START)
