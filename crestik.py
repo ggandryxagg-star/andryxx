89525a661098f1a9ff99bf0d915d4f5ca5ab05c2
ggandryxagg-star <ggandryxagg@gmail.com>
2026-03-07 14:08:11 +0500

    121

--- crestik.py
+++ crestik.py
@@ -8,15 +8,15 @@ def main(page: ft.Page):
     page.window.height = 900
     page.window.width = 900
 
-    btn = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=100, height=100, icon_size=100, alignment=ft.MainAxisAlignment.START)
-    btn1 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.CENTER)
-    btn2 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.END)
-    btn3 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.START)
-    btn4 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.CENTER)
-    btn5 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.END)
-    btn6 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.START)
-    btn7 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.CENTER)
-    btn8 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100, alignment=ft.MainAxisAlignment.END)
+    btn = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=100, height=100, icon_size=100)
+    btn1 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn2 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn3 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn4 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn5 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn6 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn7 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
+    btn8 = ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_DOWN, width=150, height=150, icon_size=100)
 
     page.add(btn, btn1, btn2)
     page.add(btn3, btn4, btn5)
