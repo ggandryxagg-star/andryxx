5b055035e40582353f293508fe2f364f24b43062
ggandryxagg-star <ggandryxagg@gmail.com>
2026-06-11 20:11:41 +0500

    aa

--- spisanka.py
+++ spisanka.py
@@ -9,7 +9,6 @@ bot = telebot.TeleBot('8928265509:AAFxL2vRWzFV5zIqwuGznTYXejt0ne7CmiU')
 
 def start(message):
 
-    bot.delete_message(message.chat.id, -1)
     bot.send_message(message.chat.id, 'Этот бот для списаний продкутов')
 
 
