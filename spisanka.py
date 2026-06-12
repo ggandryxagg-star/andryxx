5a7a40ff5f4fa93dcae1c67fec13c2cba5663c43
ggandryxagg-star <ggandryxagg@gmail.com>
2026-06-12 15:08:37 +0500

    12

--- spisanka.py
+++ spisanka.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import *
 
 
-bot = telebot.TeleBot('8928265509:AAFxL2vRWzFV5zIqwuGznTYXejt0ne7CmiU')
+bot = telebot.TeleBot('8928265509:AAENWju3gxPclv6xOHAIcFTkLP7d_N9Qocs')
 
 
 @bot.message_handler(commands=['start'])
@@ -10,6 +10,6 @@ bot = telebot.TeleBot('8928265509:AAFxL2vRWzFV5zIqwuGznTYXejt0ne7CmiU')
 def main(message):
 
     bot.send_message(message.chat.id, 'Этот бот для списаний продкутов, привета')
-
-
+    
+    
 bot.polling(none_stop=True)
